d939096452c1ab789c9c378791de444d3699443e
OracleAmongGods <137842537+OracleAmongGods@users.noreply.github.com>
2024-10-27 07:15:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
  
-#
-# pygame (simple) template - by furas
-#
-# https://github.com/furas/my-python-codes/tree/master/pygame/__template__/
-#
- 
-# ---------------------------------------------------------------------
- 
 import pygame
 import math
 from enum import Enum
@@ -21,8 +13,10 @@ RED   = (255,   0,   0)
 GREEN = (  0, 255,   0)
 BLUE  = (  0,   0, 255)
  
-SCREEN_WIDTH  = 1600
-SCREEN_HEIGHT = 900
+SCREEN_WIDTH  = 800
+SCREEN_HEIGHT = 450
+
+CURRENT_TEXT = ""
  
 BLOCK_SIZE = 50
 
@@ -34,11 +28,41 @@ class BLOCKS(Enum):
     SET_VARIABLE = 2
     DO_OP_ON_VARIABLE = 3
     OUTPUT_VARIABLE = 4
+    GET_VARIABLE_FROM_USER = 5
+    DO_RANGE = 6
+    END_REPEAT = 7
  
 # === SHENANIGANS === (CamelCase names)
+MIN_BLOCK = 2
+MAX_BLOCK = 7
+CURRENT_BLOCK_TO_SPAWN = MIN_BLOCK
+
+def inc_block():
+    global CURRENT_BLOCK_TO_SPAWN
+    global MIN_BLOCK
+    global MAX_BLOCK
+
+    CURRENT_BLOCK_TO_SPAWN += 1
+    if CURRENT_BLOCK_TO_SPAWN > MAX_BLOCK:
+        CURRENT_BLOCK_TO_SPAWN = MIN_BLOCK
+
 
 def spawn_block():
-    pass
+    global inputs, CURRENT_BLOCK_TO_SPAWN
+    arg1 = inputs[0].text
+    arg2 = inputs[1].text
+    arg3 = inputs[2].text
+
+    opts = {
+        "a1": arg1,
+        "a2": arg2,
+        "a3": arg3,
+        "type": BLOCKS(CURRENT_BLOCK_TO_SPAWN)
+    }
+
+    
+    blocks.append(Block((255, 0, 0), opts,(110, 70), (114, 58)))
+
 
 def gen_code_to_file():
     out = generate_code()
@@ -50,26 +74,80 @@ def generate_code(top_level_block=None):
     if(top_level_block is None):
         top_level_block = blocks[0]
     output = ""
-    stack = []
+    current_indent = 0
     current_block = top_level_block
     while (current_block is not None):
         print(current_block.opts["type"])
-        current_line = "\t" * (len(stack))
+        current_line = "\t" * current_indent
         if(current_block.opts["type"] is BLOCKS.START):
             current_block = current_block.child
             continue
         elif(current_block.opts["type"] is BLOCKS.SET_VARIABLE):
-            current_line += current_block.opts["var"] + "=" + str(current_block.opts["val"])
+            current_line += current_block.opts["a1"] + "=" + str(current_block.opts["a2"])
         elif(current_block.opts["type"] is BLOCKS.DO_OP_ON_VARIABLE):
-            current_line += current_block.opts["var"] + current_block.opts["op"] + "=" + str(current_block.opts["val"])
+            current_line += current_block.opts["a1"] + current_block.opts["a2"] + "=" + str(current_block.opts["a3"])
         elif(current_block.opts["type"] is BLOCKS.OUTPUT_VARIABLE):
-            current_line += "print(" + current_block.opts["var"] + ")"
+            current_line += "print(" + current_block.opts["a1"] + ")"
+        elif(current_block.opts["type"] is BLOCKS.GET_VARIABLE_FROM_USER):
+            current_line += current_block.opts['a1'] + ' =' + " int(input('Enter a Number: '))"
+        elif(current_block.opts["type"] is BLOCKS.DO_RANGE):
+            current_line += 'for ' + current_block.opts['a1'] + ' in range(' + current_block.opts['a2']+'):'
+            current_indent += 1
+        elif(current_block.opts["type"] is BLOCKS.END_REPEAT):
+            current_indent -= 1
+        
+            #how to make code that executes after this indented to signify that it is in the loop?
         output += current_line + "\n"
         current_block = current_block.child
     return output
 
+class InputBox():
+    def __init__(self, pos, txt = ""):
+        self.pos = pos
+        self.font = pygame.font.SysFont("bitstreamverasans", 24)
+        self.box = pygame.Rect(pos[0], pos[1], 600, 50)
+        self.color_inactive = pygame.Color('lightskyblue3')
+        self.color_active = pygame.Color('dodgerblue2')
+        self.active = False
+        self.text = txt
+        self.txt_surface = self.font.render(self.text, True, (0, 0, 255))
+        self.width = max(200, self.txt_surface.get_width() + 10)
+        self.bg = pygame.Surface((self.width, 50), pygame.SRCALPHA)
+        self.bg.fill((0, 0, 0))
+
+    def handle_event(self, event):
+        global inputs
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.box.collidepoint(event.pos):
+                for i in inputs:
+                    if i is not self:
+                        i.active = False
+                self.active = not self.active
+                
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                elif event.key == pygame.K_RETURN:
+                    global CURRENT_TEXT
+                    CURRENT_TEXT = self.text
+                    self.text = ""
+                else:
+                    self.text += event.unicode
+
+        self.txt_surface = self.font.render(self.text, True, pygame.Color('white'))
+        self.width = max(200, self.txt_surface.get_width() + 10)
+        self.bg = pygame.Surface((self.width, 50), pygame.SRCALPHA)
+        self.bg.fill((0, 0, 0))
+
+    def draw(self, screen):
+        screen.blit(self.bg, (self.box.x, self.box.y))
+        screen.blit(self.txt_surface, (self.box.x, self.box.y))
+
+
 class Block():
-    def __init__(self, col, opts={"type":BLOCKS.DEBUG}, pos=(0,0), size=(100,50), parent=None):
+    def __init__(self, col, opts={"type":BLOCKS.START}, pos=(0,0), size=(100,50), parent=None):
         self.col = col
         self.opts = opts
         
@@ -80,18 +158,6 @@ class Block():
         # self.text = opts["type"]
         self.type = opts["type"]
         self.rect = pygame.Rect(pos, size)
- 
-        # self.image_normal = pygame.Surface(size)
-        # self.image_normal.blit(self.spr, (0,0), pygame.Rect((0,0), size))
-        # txt_image = self.font.render(self.text, True, BLACK)
-        # txt_rect = txt_image.get_rect()
-        # self.image_normal.blit(txt_image, txt_rect)
-       
-        # self.image_hover = pygame.Surface(size)
-        # self.image_hover.blit(self.spr, (0,0), pygame.Rect((0,0), size))
-        # txt_image = self.font.render(self.text, True, WHITE)
-        # txt_rect = txt_image.get_rect(center=self.rect.center)
-        # self.image_hover.blit(txt_image, txt_rect)
 
         self.rect.topleft = pos
         self.pos = pos
@@ -110,11 +176,17 @@ class Block():
         if(self.opts["type"] is BLOCKS.START):
             text = "Start"
         elif(self.opts["type"] is BLOCKS.SET_VARIABLE):
-            text = "Set " + self.opts["var"] + " to " + str(self.opts["val"])
+            text = "Set " + self.opts["a1"] + " to " + str(self.opts["a2"])
         elif(self.opts["type"] is BLOCKS.DO_OP_ON_VARIABLE):
-            text = self.opts["var"] + " = " + self.opts["var"] + " " + self.opts["op"] + " " + str(self.opts["val"])
+            text = self.opts["a1"] + " = " + self.opts["a1"] + " " + self.opts["a2"] + " " + str(self.opts["a3"])
         elif(self.opts["type"] is BLOCKS.OUTPUT_VARIABLE):
-            text = "Output " + self.opts["var"] + " to the console"
+            text = "Output " + self.opts["a1"] + " to the console"
+        elif(self.opts["type"] is BLOCKS.GET_VARIABLE_FROM_USER):
+            text = "Put a user number in " + self.opts["a1"]
+        elif(self.opts["type"] is BLOCKS.DO_RANGE):
+            text = 'Repeat ' + self.opts['a1'] + " times"
+        elif(self.opts["type"] is BLOCKS.END_REPEAT):
+            text = "End Repeat"
         self.text = text
         self.resize_image()
 
@@ -203,21 +275,19 @@ class Button():
                 if event.button == 1:
                     self.command()
 
-# === MAIN === (lower_case names)
- 
-# --- (global) variables ---
- 
-# --- init ---
-
-
- 
 pygame.init()
 pygame.font.init()
-DEFAULT_FONT = pygame.font.SysFont('bitstreamverasans', 36)
 
 
-spawn_block_button = Button(text="TEST", pos=(0,0), command=spawn_block) # create button and assign function
-test_button = Button(text="TEST", pos=(110,0), command=gen_code_to_file) # create button and assign function
+test_button = Button(text="TEST", pos=(0,0), command=gen_code_to_file) # create button and assign function
+spawn_block_button = Button(text="SPAWN", pos=(110,0), command=spawn_block) # create button and assign function
+change_block_button = Button(text="CHANGE", pos=(220,0), command=inc_block) # create button and assign function
+
+inputs = [
+    InputBox((5, SCREEN_HEIGHT-165), "X"),
+    InputBox((5, SCREEN_HEIGHT-110), "*"),
+    InputBox((5, SCREEN_HEIGHT-55), "5")
+]
  
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 screen_rect = screen.get_rect()
@@ -228,27 +298,27 @@ blocks.append(Block((255, 0, 0), {
     "type":  BLOCKS.START
 },(200, 100), (114, 58)))
 
-blocks.append(Block((255, 0, 0), {
-    "type":  BLOCKS.SET_VARIABLE,
-    "var": "x",
-    "val": 5
-},(200, 100), (114, 58)))
+# blocks.append(Block((255, 0, 0), {
+#     "type":  BLOCKS.SET_VARIABLE,
+#     "var": "x",
+#     "val": 5
+# },(200, 100), (114, 58)))
 
-blocks.append(Block((255, 0, 0), {
-    "type":  BLOCKS.DO_OP_ON_VARIABLE,
-    "var": "x",
-    "op": "*",
-    "val": "x"
-},(200, 100), (114, 58)))
+# blocks.append(Block((255, 0, 0), {
+#     "type":  BLOCKS.DO_OP_ON_VARIABLE,
+#     "var": "x",
+#     "op": "*",
+#     "val": "x"
+# },(200, 100), (114, 58)))
 
-blocks.append(Block((255, 0, 0), {
-    "type":  BLOCKS.OUTPUT_VARIABLE,
-    "var": "x",
-},(200, 100), (114, 58)))
+# blocks.append(Block((255, 0, 0), {
+#     "type":  BLOCKS.OUTPUT_VARIABLE,
+#     "var": "x",
+# },(200, 100), (114, 58)))
 
-blocks[1].connect_to(blocks[0])
-blocks[2].connect_to(blocks[1])
-blocks[0].move_to((200, 100))
+# blocks[1].connect_to(blocks[0])
+# blocks[2].connect_to(blocks[1])
+# blocks[0].move_to((200, 100))
 
 # - drag -
 
@@ -285,6 +355,11 @@ while is_running:
                         selected = i
                         selected_offset_x = s.pos[0] - event.pos[0]
                         selected_offset_y = s.pos[1] - event.pos[1]
+            elif event.button == 3:
+                for i, s in enumerate(blocks):
+                    if(s.rect.collidepoint(event.pos)):
+                        blocks.remove(s)
+                        break
                
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
@@ -306,22 +381,27 @@ while is_running:
         # --- objects events ---
         
         test_button.handle_event(event)
+        spawn_block_button.handle_event(event)
+        change_block_button.handle_event(event)
        
-    # --- updates ---
- 
-        # empty
-       
+        for s in inputs:
+            s.handle_event(event)
     # --- draws ---
    
     screen.fill((64, 64, 64))
 
     
     test_button.draw(screen) 
+    spawn_block_button.draw(screen) 
+    change_block_button.draw(screen) 
     
    
     # draw rect
     i = 1
     for s in blocks:
+        s.draw(screen)
+    
+    for s in inputs:
         s.draw(screen)
        
     pygame.display.update()
